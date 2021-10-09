5f1a81307d581d63ce081e05bac6b6dfbc12cdb4
yogesh <malkoti.yogesh@gmail.com>
2021-10-09 13:12:24 +0800

    full final

--- mymodules/hello/go.mod
+++ mymodules/hello/go.mod
@@ -2,3 +2,4 @@ module github.com/yogeshmalkoti/go/mymodules/hello
 
 go 1.16
 
+require github.com/yogeshmalkoti/go/mymodules/greetings v0.0.0-20211009050649-34187d8f9020
--- mymodules/hello/hello.go
+++ mymodules/hello/hello.go
@@ -11,7 +11,7 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 	// A slice of names.
-	names := []string{"Gladys", "Samantha", "Darrin"}
+	names := []string{"Yogesh", "Amit", "Anil", "Sandeep"}
 	message, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
